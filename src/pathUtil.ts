d0dd9833c4ea30df6e06d4e53cb86e64be5a39ac
bojun1995 <bojun1995@foxmail.com>
2022-06-29 23:15:34 +0800

    [chg]rename

--- src/pathUtil.ts
+++ src/pathUtil.ts
@@ -1,14 +1,1 @@
 import * as vscode from 'vscode'
-
-export const getRelativePath = (uri: string | undefined) => {
-  if (typeof uri === 'undefined') {
-    if (vscode.window.activeTextEditor) {
-      uri = vscode.window.activeTextEditor.document.uri as unknown as string
-    }
-  }
-  if (!uri) {
-    vscode.window.showErrorMessage('未能获取到文件路径')
-    return
-  }
-  // const path = vscode.workspace.asRelativePath(uri)
-}
