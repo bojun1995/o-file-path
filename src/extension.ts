ff5f1c35ab70cdb8808514accca918319a86eb23
bojun1995 <bojun1995@foxmail.com>
2022-06-29 23:54:23 +0800

    [chg]path util

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,23 @@
 import * as vscode from 'vscode'
+import { getRealPath } from './pathUtil'
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand(
-    'o-relative-path.getRelativePath',
-    (uri: vscode.Uri | undefined) => {
-      debugger
-      // if (typeof uri === 'undefined') {
-      //   if (vscode.window.activeTextEditor) {
-      //     uri = vscode.window.activeTextEditor.document.uri
-      //   }
-      // }
-      // if (!uri) {
-      //   vscode.window.showErrorMessage('未能获取到文件路径')
-      //   return
-      // }
-      // let path = vscode.workspace.asRelativePath(uri)
-      // path = path.replace(/\\/g, '/')
-      // vscode.window.showInformationMessage(path)
-    }
-  )
+  const disposable = vscode.commands.registerCommand('o-file-path.getAliasPath', (uri: vscode.Uri | undefined) => {
+    const realUri = getRealPath(uri)
+    vscode.window.showInformationMessage(`realUri = ${realUri}`)
+    // if (typeof uri === 'undefined') {
+    //   if (vscode.window.activeTextEditor) {
+    //     uri = vscode.window.activeTextEditor.document.uri
+    //   }
+    // }
+    // if (!uri) {
+    //   vscode.window.showErrorMessage('未能获取到文件路径')
+    //   return
+    // }
+    // let path = vscode.workspace.asRelativePath(uri)
+    // path = path.replace(/\\/g, '/')
+    // vscode.window.showInformationMessage(path)
+  })
   context.subscriptions.push(disposable)
 }
 
--- src/pathUtil.ts
+++ src/pathUtil.ts
@@ -1,1 +1,19 @@
 import * as vscode from 'vscode'
+
+export const getRealPath = (uri: vscode.Uri | undefined): string => {
+  const ret = {
+    path: '',
+  }
+  // 通过command直接调用
+  if (typeof uri == undefined) {
+    if (vscode.window.activeTextEditor) {
+      ret.path = vscode.window.activeTextEditor.document.uri as unknown as string
+    }
+  }
+
+  // 通过左侧文件栏菜单或文件顶部菜单调用
+  if (uri instanceof vscode.Uri) {
+    ret.path = vscode.workspace.asRelativePath(uri)
+  }
+  return ret.path
+}
